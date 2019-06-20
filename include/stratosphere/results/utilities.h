7c9df9cfd8293c01934050ceafc1213622645fee
Michael Scire <SciresM@gmail.com>
2019-06-20 02:00:33 -0700

    results: Add R_ASSERT helper.

--- include/stratosphere/results/utilities.h
+++ include/stratosphere/results/utilities.h
@@ -20,6 +20,21 @@ extern "C" {
         } \
     })
 
+/// Evaluates an expression that returns a result, and fatals the result if it would fail.
+#ifdef RESULT_ABORT_ON_ASSERT
+#define R_ASSERT_IMPL(res) std::abort()
+#else
+#define R_ASSERT_IMPL(res) fatalSimple(res)
+#endif
+
+#define R_ASSERT(res_expr) \
+    ({ \
+        const Result _tmp_r_assert_rc = res_expr; \
+        if (R_FAILED(_tmp_r_assert_rc)) {  \
+            R_ASSERT_IMPL(_tmp_r_assert_rc); \
+        } \
+    })
+
 /// Helpers for pattern-matching on a result expression, if the result would fail.
 #define R_TRY_CATCH(res_expr) \
     ({ \
@@ -44,6 +59,11 @@ extern "C" {
 #define R_CATCH_MODULE(module) \
             } else if (R_MODULE(_tmp_r_try_catch_rc) == module) { \
                 _Static_assert(module != 0, "Catch module must be error!"); \
+                if (false) { } \
+                else
+
+#define R_CATCH_ALL() \
+            } else if (R_FAILED(_tmp_r_try_catch_rc)) { \
                 if (false) { } \
                 else
 
